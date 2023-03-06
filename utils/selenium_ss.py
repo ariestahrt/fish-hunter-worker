957de4aa58e6a90ef6a805f977a856aa1359c787
Ariesta <ariesta.19051@mhs.its.ac.id>
2023-03-06 11:44:30 +0800

    Fix: chromedriver error

--- utils/selenium_ss.py
+++ utils/selenium_ss.py
@@ -28,20 +28,20 @@ prefs["profile.managed_default_content_settings.javascript"] = 2
 
 options.add_experimental_option("prefs", prefs)
 
-driver = webdriver.Chrome(service=Service("/usr/local/bin/chromedriver"), options=options, desired_capabilities=DesiredCapabilities.CHROME)
-
-driver.maximize_window()
-driver.set_window_size(1920, 1080)
-
 def screenshot(url, save_to="test_ss.png"):
-    global driver
-    driver.get(url)    
+    driver = webdriver.Chrome(service=Service("/usr/local/bin/chromedriver"), options=options, desired_capabilities=DesiredCapabilities.CHROME)
+
+    driver.maximize_window()
+    driver.set_window_size(1920, 1080)
+    driver.get(url)
     ss = driver.get_screenshot_as_png()
 
     # convert to jpg
     img = Image.open(BytesIO(ss))
     img = img.convert("RGB")
     img.save(save_to)
+
+    driver.quit()
 
 if __name__ == "__main__":
     screenshot("https://www.whatismybrowser.com/detect/is-javascript-enabled", save_to="test_ss.jpg")
