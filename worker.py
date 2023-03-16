e135bab40038e1526aa39f2df3438c723564390a
Ariesta <ikadekagusariestaputra@gmail.com>
2023-03-16 13:52:36 +0800

    Fix: url already in db

--- worker.py
+++ worker.py
@@ -199,7 +199,7 @@ def save_dataset(uuid, fish_id):
         return Exception("URLScan data not found"), dataset_info, urlscan_data
     
     # check is url already in db
-    if DATASETS.find({"url": urlscan_data["page"]["url"]}).count() > 0:
+    if DATASETS.find({"url": urlscan_data["page"]["url"]}) != None:
         logging.error(">>>> URL already in db")
         dataset_info["reject_details"] = "URL already in db"
         return Exception("URL already in db"), dataset_info, urlscan_data
